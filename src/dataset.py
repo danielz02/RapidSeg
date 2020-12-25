ebef35771da95a4c7ef49340a8140069aece59de
danielz02 <zchdz0215@gmail.com>
2020-12-25 17:37:21 -0600

    Add comments for the dataloader

--- src/dataset.py
+++ src/dataset.py
@@ -10,6 +10,13 @@ class RapidEye(data.Dataset):
             self, img_dir: str, mask_dir: str or None, band: str,
             transform: Compose = None  # Albumentations transformation objects
     ):
+        """
+        Instantiate a dataloader for the RapidEye dataset
+        :param img_dir: The path to the directory of GeoTiff images to load
+        :param mask_dir: The path to the directory of GeoTiff mask to load
+        :param band: The number of bands to load (RGB or Full bands)
+        :param transform: Object that implements torchvision.transforms API
+        """
         super().__init__()
         assert band is "RGB" or "Full"
 
@@ -25,13 +32,14 @@ class RapidEye(data.Dataset):
         img_path = os.path.join(self.img_dir, os.listdir(self.img_dir)[index])
         img_raster = rasterio.open(img_path)
         img_arr = img_raster.read()
-        _, h, w = img_arr.shape
+        _, h, w = img_arr.shape  # Rasterio loads raster in CHW format
         img_raster.close()
 
+        # Convert to int64 so that transformations can be applied as required by albumentations
         img_arr = img_arr.astype(np.longlong)
         if self.band is "RGB":
             img_arr = img_arr[0:3, :].copy()
-        img_arr = img_arr.transpose(1, 2, 0)
+        img_arr = img_arr.transpose(1, 2, 0)  # Convert to HWC
         if not self.mask_dir:
             return (self.transform(image=img_arr)["image"], None) if self.transform else (img_arr, None)
 
@@ -40,10 +48,11 @@ class RapidEye(data.Dataset):
         mask_arr = mask_raster.read()
         mask_raster.close()
 
+        # Encode new labels: 0 -> Others | 1 -> Corn | 2 -> Soybean
         mask_arr = mask_arr.astype(np.longlong)
         mask_arr[np.logical_and(mask_arr != 1, mask_arr != 5)] = 0
         mask_arr[mask_arr == 5] = 2
         mask_arr = mask_arr.reshape(h, w)
 
-        transformed = self.transform(image=img_arr, masks=[mask_arr])
+        transformed = self.transform(image=img_arr, masks=[mask_arr])  # data augmentation for image and mask
         return (transformed["image"], transformed["masks"][0]) if self.transform else (img_arr, mask_arr)
