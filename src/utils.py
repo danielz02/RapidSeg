c976a772421f68d505a6c6ef7fd04bd66406d3a9
danielz02 <zchdz0215@gmail.com>
2020-12-25 17:50:22 -0600

    Add comments for the visualization utilities

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,12 @@ from src.params import COLOR_MAP
 from skimage.color import label2rgb
 
 
-def normalize(arr: np.ndarray):
+def normalize(arr: np.ndarray or torch.Tensor):
+    """
+    Normalize a single channel to [0, 1]
+    :param arr: A slice of the channel to normalize
+    :return: normalized channel
+    """
     arr_min, arr_max = np.min(arr), np.max(arr)
     return (arr - arr_min) / (arr_max - arr_min)
 
@@ -15,6 +20,13 @@ def visualize_preprocess(
     mask_batch_target: np.ndarray or torch.Tensor,
     mask_batch_pred: np.ndarray or torch.Tensor = None
 ):
+    """
+    Take in a batch of images in NCHW, choose one image, and convert to HWC
+    :param img_batch: the batch of images (in NCHW) to preprocess
+    :param mask_batch_target: the batch of masks (in NHW)
+    :param mask_batch_pred: the batch of predicted masks (in NHW)
+    :return:
+    """
     if isinstance(img_batch, torch.Tensor):
         img_batch = img_batch.cpu().numpy()
     if isinstance(mask_batch_target, torch.Tensor):
@@ -27,6 +39,7 @@ def visualize_preprocess(
     random_img = img_batch[rand_idx, :]
     random_target = mask_batch_target[rand_idx, :]
 
+    # Keep RGB channels only
     if c > 3:
         random_img = random_img[:3, :]
     random_img[0, :] = normalize(random_img[0, :])
@@ -45,6 +58,11 @@ def visualize_sample(
     img_batch: np.ndarray or torch.Tensor,
     mask_batch_target: np.ndarray or torch.Tensor,
 ):
+    """
+    Display one image in the batch with mask overlaid
+    :param img_batch: the batch of images in NCHW to visualize
+    :param mask_batch_target: the batch of corresponding mask in NHW to visualize
+    """
     img, mask_target = visualize_preprocess(img_batch, mask_batch_target)
 
     _, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 8))
@@ -52,6 +70,7 @@ def visualize_sample(
     ax[1].imshow(overlay_mask(mask_target, img))
     ax[0].set_title("Random Image in the Batch")
     ax[1].set_title("Overlaid Target Mask")
+    plt.show()
 
 
 def visualize_pred(
@@ -59,10 +78,20 @@ def visualize_pred(
     mask_batch_pred: np.ndarray or torch.Tensor,
     mask_batch_target: np.ndarray or torch.Tensor,
 ):
+    """
+    Plot one image in the batch, the image overlaid with the ground truth mask,
+    the image overlaid with the predicted mask, and the image overlaid with the predicted mask with
+    wrong classifications mark red
+    :param img_batch: the batch of images in NCHW to visualize
+    :param mask_batch_pred: the batch of corresponding mask in NHW to visualize
+    :param mask_batch_target: the batch of predicted masks (in NHW)
+    :return:
+    """
     img, mask_target, mask_pred = visualize_preprocess(img_batch, mask_batch_target, mask_batch_pred)
     mask_pred_overlay = overlay_mask(mask_pred, img)
     mask_target_overlay = overlay_mask(mask_target, img)
     mask_miss_classifications_overlay = mask_target_overlay.copy()
+    # Mark wrong classifications as red
     mask_miss_classifications_overlay[mask_target != mask_pred, :] = torch.tensor([1., 0., 0.])
 
     _, ax = plt.subplots(nrows=2, ncols=2, figsize=(8, 8))
@@ -74,9 +103,16 @@ def visualize_pred(
     ax[0, 1].set_title("Overlaid Target Mask")
     ax[1, 0].set_title("Overlaid Prediction Mask")
     ax[1, 1].set_title("Overlaid Miss-Classification Mask")
+    plt.show()
 
 
 def overlay_mask(mask: np.ndarray, img: np.ndarray = None) -> np.ndarray:
+    """
+    Overlay mask to the given image with the color map specified in CDL_2013_clip_20170525181724_1012622514.tif.vat.dbf
+    :param mask: Mask of the corresponding image in HW
+    :param img: Image to be displayed in HWC
+    :return: An image tensor with mask overlaid. The "Others" class is ignored
+    """
     if img is not None:
         *_, c = img.shape
         assert c == 3
